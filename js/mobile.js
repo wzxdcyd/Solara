cab825e8b444e538d2a1bc902cf4bd6610055365
tomas <2567088097@qq.com>
2026-01-01 16:19:29 +0800

    fix: update mobile toolbar title to Meol Player

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "Meol Player";
     }
 
     function updateMobileOverlayScrim() {
